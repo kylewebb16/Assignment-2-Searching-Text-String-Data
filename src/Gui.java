ef4f7940e085eebb3dbbb34d28bd93f4f18462df
kylewebb16 <129976977+kylewebb16@users.noreply.github.com>
2024-11-27 19:18:28 -0500

    Fixed up documentation

--- src/Gui.java
+++ src/Gui.java
@@ -1,13 +1,10 @@
 import javax.swing.*;
 
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
-public class Gui {
+public class Gui extends JFrame{
     public Gui() {
         JFrame mainFrame = new JFrame("String Search");
         mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -16,22 +13,24 @@ public class Gui {
 
         createInstructionsPanel(mainFrame);
         createButtons(mainFrame);
-    
-        mainFrame.pack();
+
+
+        mainFrame.setSize(1024, 576);
         mainFrame.setVisible(true);
     }
 
     
     private static void createInstructionsPanel(JFrame mainFrame) {
         JLabel instructionsLabel = new JLabel();
-        instructionsLabel.setText("<html><h2>Use the buttons to display the provided text, search for text in the program, and exit the program<h2></html>");
+        instructionsLabel.setText("<html><h1>Use the buttons to:</h1></html>");
+        instructionsLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        instructionsLabel.setBorder(BorderFactory.createLineBorder(Color.blue));
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.gridwidth = 2;
-        gbc.ipadx = 10;
+        gbc.gridwidth = 3;
         gbc.insets = new Insets(5, 20, 5, 20);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = GridBagConstraints.BOTH;
         gbc.anchor = GridBagConstraints.PAGE_START;
         mainFrame.getContentPane().add(instructionsLabel, gbc);
     }
@@ -39,74 +38,109 @@ public class Gui {
     private static class MyButton extends JButton{
         public MyButton(ImageIcon icon, String buttonText){
             this.setText("<html><h2>" + buttonText + "</h2></html>");
-            this.setIcon(icon);
-            this.setPreferredSize(new Dimension(200, 70));
-            this.setIconTextGap(20);
-            this.setHorizontalAlignment(SwingConstants.LEFT);
+            this.setPreferredSize(new Dimension(200, 50));
+            this.setHorizontalAlignment(SwingConstants.CENTER);
             this.setFocusPainted(false);
         }
     }
     
     private static void createButtons(JFrame mainFrame) {
-        
-        MyButton printButton1 = new MyButton(new ImageIcon("../images/keyboard (1).png"), "Print Text");
+
+        // Create print button at grid (x,y) = (0,1)
+        MyButton printButton1 = new MyButton(new ImageIcon("../images/keyboard (1).png"), "1: Print Text");
         GridBagConstraints b1Constraints = new GridBagConstraints();
-        b1Constraints.gridx = 1;
+        b1Constraints.gridx = 0;
         b1Constraints.gridy = 1;
-        b1Constraints.insets = new Insets(2, 0, 2, 0);
+        b1Constraints.insets = new Insets(16, 32, 16, 32);
         mainFrame.getContentPane().add(printButton1, b1Constraints);
-        
-        
-        MyButton searchButton2 = new MyButton(new ImageIcon("../images/keyboard (2).png"), "Search Text");
+
+
+        // Create search button at grid (x,y) = (1,1)
+        MyButton searchButton2 = new MyButton(new ImageIcon("../images/keyboard (2).png"), "2: Search Text");
         GridBagConstraints b2Constraints = new GridBagConstraints();
         b2Constraints.gridx = 1;
-        b2Constraints.gridy = 2;
-        b2Constraints.insets = new Insets(2, 0, 2, 0);
+        b2Constraints.gridy = 1;
+        b2Constraints.insets = new Insets(16, 32, 16, 32);
         mainFrame.getContentPane().add(searchButton2, b2Constraints);
-        
-        
-        MyButton exitButton3 = new MyButton(new ImageIcon("../images/keyboard (3).png"), "Exit Program");
+
+
+        // Create exit button at grid (x,y) = (2,1)
+        MyButton exitButton3 = new MyButton(new ImageIcon("../images/keyboard (3).png"), "3: Exit Program");
         GridBagConstraints b3Constraints = new GridBagConstraints();
-        b3Constraints.gridx = 1;
-        b3Constraints.gridy = 3;
-        b3Constraints.insets = new Insets(2, 0, 2, 0);
+        b3Constraints.gridx = 2;
+        b3Constraints.gridy = 1;
+        b3Constraints.insets = new Insets(16, 32, 16, 32);
         mainFrame.getContentPane().add(exitButton3, b3Constraints);
 
+        // Create actions
+
+        // Create and map action for print button
         Action printAction = new AbstractAction() {
+            // Shows the searchable text and disables the print button
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                printButton1.setEnabled(false);
                 createOutputText(mainFrame);
             }
         };
+        // Map action to keybinding and button
         printButton1.addActionListener(printAction);
-        printButton1.getInputMap().put(KeyStroke.getKeyStroke("1"), "printText");
+        printButton1.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("1"), "printText");
         printButton1.getActionMap().put("printText", printAction);
+
+        // Create and map action for searchButton
+        Action searchAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String searchTerm = JOptionPane.showInputDialog(mainFrame, "Enter a search term");
+                if (searchTerm != null) {
+                    ArrayList<Integer> results = StringSearch.BMAlgorithm(getStatesString(), searchTerm);
+                    if (results.isEmpty()) JOptionPane.showMessageDialog(mainFrame, "No occurances of '" + searchTerm + "' found.");
+                    else JOptionPane.showMessageDialog(mainFrame, "Found '" + searchTerm + "' at index\n" + results);
+                }
+            }
+        };
+        searchButton2.addActionListener(searchAction);
+        searchButton2.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("2"), "searchText");
+        searchButton2.getActionMap().put("searchText", searchAction);
+
+        Action exitAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int choice = JOptionPane.showConfirmDialog(mainFrame, "Do you want to exit the program?", "Exit Program?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
+                if (choice == JOptionPane.YES_OPTION) System.exit(0);
+            }
+        };
+        exitButton3.addActionListener(exitAction);
+        exitButton3.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("3"), "exit");
+        exitButton3.getActionMap().put("exit", exitAction);
     }
 
     private static void createOutputText(JFrame mainFrame) {
         Font outputFont = new Font("Arial Bold", Font.BOLD, 15);
 
-        JTextArea outputEditorPane = new JTextArea();
+        JTextPane outputEditorPane = new JTextPane();
         outputEditorPane.setText("String Contents:\n" + getStatesString());
         outputEditorPane.setFont(outputFont);
         outputEditorPane.setOpaque(false);
-        outputEditorPane.setLineWrap(true);
-        outputEditorPane.setWrapStyleWord(true);
+//        outputEditorPane.setLineWrap(true);
+//        outputEditorPane.setWrapStyleWord(true);
         outputEditorPane.setEditable(false);
-        outputEditorPane.setPreferredSize(new Dimension(400, 140));
+        outputEditorPane.setPreferredSize(new Dimension(400, 155));
+        outputEditorPane.setBorder(BorderFactory.createLineBorder(Color.red));
 
         // Positioning row 2, column 0, fill horizontal
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
-        gbc.gridy = 4;
+        gbc.gridy = 2;
         gbc.gridwidth = 3;
         gbc.ipadx = 10;
         gbc.insets = new Insets(5, 20, 5, 20);
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.anchor = GridBagConstraints.PAGE_END;
 
         mainFrame.getContentPane().add(outputEditorPane, gbc);
-        mainFrame.pack();
+//        mainFrame.pack();
     }
 
     
--- src/StringSearch.java
+++ src/StringSearch.java
@@ -1,36 +1,44 @@
-import java.lang.reflect.Array;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Arrays;
 
 public class StringSearch {
     String text = "the dog and the man go to the park";
 
+    // Preforms the text search for search term using bad-character rule
     public static ArrayList<Integer> BMAlgorithm(String text, String searchKey) {
+        // Set text and search inputs as char[] to iterate over
         ArrayList<Integer> results = new ArrayList<>();
         char[] textArray = text.toCharArray();
         char[] searchArray = searchKey.toCharArray();
+        int searchLength = searchArray.length;
+        // Adds chars in search term to a hash map to remember the last occurrence
         HashMap<Character, Integer> badChars = computeBadCharacter(searchArray);
         int textLength = textArray.length;
-        int searchLength = searchArray.length;
+        // Declare text search pointer
         int i = 0;
+
+        // Search through text until i is beyond possible range
         while (i < textLength - searchLength) {
+            // Declare search key pointer to last char in term (to check from right to left and perform skips)
             int j = searchLength - 1;
+            // Check if last char in search term matches corresponding char in text, move left if so
             while (j >= 0 && searchArray[j] == textArray[i + j]) {
                 j--;
             }
+            // If j == -1, every char matched, we found a match! add to results
             if (j == -1) {
                 results.add(i);
                 i++;
             }
+            // Mismatch found, check if bad character found
             else {
-                // If pattern contains the current character
-                    // add occurance to textptr
-                // else shift textptr entire searchkey length
                 char currentChar = textArray[i + j];
-// ERROR HERE
-                int lastOccurance = badChars.getOrDefault(currentChar, -1);
-                // j - lastOccurance either shifts to
-                i += Math.max(j - lastOccurance, 1);
+                // If mismatched char does not exist in search key, skip past it
+                int lastOccur = badChars.getOrDefault(currentChar, -1);
+
+                i += Math.max(j - lastOccur, 1);
             }
         }
 
@@ -50,4 +58,20 @@ public class StringSearch {
         return badChars;
     }
 
+//    public static int[] goodSuffixTable(String searchKey) {
+//        int length = searchKey.length();
+//        int[] suffixShift = new int[length];
+//        Arrays.fill(suffixShift, length);
+//        for (int i = length - 1; i > 0; i--) {
+//            String suffix = searchKey.substring(i);
+//            for (int j = i - 1; j > 0; j--) {
+//                if (searchKey.substring(j).startsWith(suffix)) {
+//                    //             dist from end  -  suffix length
+//                    suffixShift[i] = (length - j) - (length - i);
+//                }
+//            }
+//
+//        }
+//        return suffixShift;
+//    }
 }
